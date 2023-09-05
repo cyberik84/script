264b189b445699b1ac89fc5949776460c7aa3a5c
cyberik84 <71454448+cyberik84@users.noreply.github.com>
2023-09-05 11:17:08 +0200

    Update chords.js

--- chords.js
+++ chords.js
@@ -1,13 +1,13 @@
-let Cmin = chooseCycles( "<Cm Eb Fm Ab>/4" ,  "<D# F#m G# B#m>/4" ,  "<Gm Bb Cm Eb>/4" ,  "<Fm Ab Bbm Db>/4" ).chord()
+let Cmin = ( "<Cm Eb Fm Ab>/4" ,  "<D# F#m G# B#m>/4" ,  "<Gm Bb Cm Eb>/4" ,  "<Fm Ab Bbm Db>/4" )
 
-let Dmin = chooseCycles( "<Dm F Gm Bb>/4" ,  "<F Am Bb Dm>/4" ,  "<Am C Dm F>/4" ,  "<Gm Bb Cm Eb>/4" ).chord()
+let Dmin = ( "<Dm F Gm Bb>/4" ,  "<F Am Bb Dm>/4" ,  "<Am C Dm F>/4" ,  "<Gm Bb Cm Eb>/4" )
 
-let Emin = chooseCycles( "<Em G Am C>/4" ,  "<G Bm C Em>/4" ,  "<Bm D Em G>/4" ,  "<Am C Dm F>/4" ).chord()
+let Emin = ( "<Em G Am C>/4" ,  "<G Bm C Em>/4" ,  "<Bm D Em G>/4" ,  "<Am C Dm F>/4" )
 
-let Fmin = chooseCycles( "<Fm Ab Bbm Db>/4" ,  "<G# B#m C# E#m>/4" ,  "<Cm Eb Fm Ab>/4" ,  "<A#m C# D#m F#>/4" ).chord()
+let Fmin = ( "<Fm Ab Bbm Db>/4" ,  "<G# B#m C# E#m>/4" ,  "<Cm Eb Fm Ab>/4" ,  "<A#m C# D#m F#>/4" )
 
-let Gmin = chooseCycles( "<Gm Bb Cm Eb>/4" ,  "<A# C#m D# F#m>/4" ,  "<Dm F Gm Bb>/4" ,  "<Cm Eb Fm Ab>/4" ).chord()
+let Gmin = ( "<Gm Bb Cm Eb>/4" ,  "<A# C#m D# F#m>/4" ,  "<Dm F Gm Bb>/4" ,  "<Cm Eb Fm Ab>/4" )
 
-let Amin = chooseCycles( "<Am C Dm F>/4" ,  "<C Em F Am>/4" ,  "<Em G Am C>/4" ,  "<Dm F Gm Bb>/4" ).chord()
+let Amin = ( "<Am C Dm F>/4" ,  "<C Em F Am>/4" ,  "<Em G Am C>/4" ,  "<Dm F Gm Bb>/4" )
 
-let Bmin = chooseCycles( "<Bm D Em G>/4" ,  "<D F#m G Bm>/4" ,  "<F#m A Bm D>/4" ,  "<Em G Am C>/4" ).chord()
+let Bmin = ( "<Bm D Em G>/4" ,  "<D F#m G Bm>/4" ,  "<F#m A Bm D>/4" ,  "<Em G Am C>/4" )
