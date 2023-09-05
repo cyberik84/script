20a72d1fdf2ec5b9a36b9cbebde2987cf17d3e05
cyberik84 <71454448+cyberik84@users.noreply.github.com>
2023-09-05 14:21:26 +0200

    Update chords.js

--- chords.js
+++ chords.js
@@ -1,13 +1,13 @@
-const Cmin = ( "<Cm Eb Fm Ab>/4" ,  "<D# F#m G# B#m>/4" ,  "<Gm Bb Cm Eb>/4" ,  "<Fm Ab Bbm Db>/4" )
+export const Cmin = ( "<Cm Eb Fm Ab>/4" ,  "<D# F#m G# B#m>/4" ,  "<Gm Bb Cm Eb>/4" ,  "<Fm Ab Bbm Db>/4" );
 
-const Dmin = ( "<Dm F Gm Bb>/4" ,  "<F Am Bb Dm>/4" ,  "<Am C Dm F>/4" ,  "<Gm Bb Cm Eb>/4" )
+export const Dmin = ( "<Dm F Gm Bb>/4" ,  "<F Am Bb Dm>/4" ,  "<Am C Dm F>/4" ,  "<Gm Bb Cm Eb>/4" ):
 
-const Emin = ( "<Em G Am C>/4" ,  "<G Bm C Em>/4" ,  "<Bm D Em G>/4" ,  "<Am C Dm F>/4" )
+export const Emin = ( "<Em G Am C>/4" ,  "<G Bm C Em>/4" ,  "<Bm D Em G>/4" ,  "<Am C Dm F>/4" ):
 
-const Fmin = ( "<Fm Ab Bbm Db>/4" ,  "<G# B#m C# E#m>/4" ,  "<Cm Eb Fm Ab>/4" ,  "<A#m C# D#m F#>/4" )
+export const Fmin = ( "<Fm Ab Bbm Db>/4" ,  "<G# B#m C# E#m>/4" ,  "<Cm Eb Fm Ab>/4" ,  "<A#m C# D#m F#>/4" ):
 
-const Gmin = ( "<Gm Bb Cm Eb>/4" ,  "<A# C#m D# F#m>/4" ,  "<Dm F Gm Bb>/4" ,  "<Cm Eb Fm Ab>/4" )
+export const Gmin = ( "<Gm Bb Cm Eb>/4" ,  "<A# C#m D# F#m>/4" ,  "<Dm F Gm Bb>/4" ,  "<Cm Eb Fm Ab>/4" ):
 
-const Amin = ( "<Am C Dm F>/4" ,  "<C Em F Am>/4" ,  "<Em G Am C>/4" ,  "<Dm F Gm Bb>/4" )
+export const Amin = ( "<Am C Dm F>/4" ,  "<C Em F Am>/4" ,  "<Em G Am C>/4" ,  "<Dm F Gm Bb>/4" ):
 
-const Bmin = ( "<Bm D Em G>/4" ,  "<D F#m G Bm>/4" ,  "<F#m A Bm D>/4" ,  "<Em G Am C>/4" )
+export const Bmin = ( "<Bm D Em G>/4" ,  "<D F#m G Bm>/4" ,  "<F#m A Bm D>/4" ,  "<Em G Am C>/4" ):
